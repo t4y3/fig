6a0b297620cb678c0da8210cc56908259c49efb9
t4y3 <y.teraoka8@gmail.com>
2018-03-31 14:54:28 +0900

    Add: shortcuts dialog

--- src/FigHeader.js
+++ src/FigHeader.js
@@ -1,9 +1,29 @@
 import { h, app } from "hyperapp"
 
+const openDialog = () => {
+  let dialog = document.getElementById('modal-shortcuts');
+  dialog.showModal();
+};
+
+const closeDialog = () => {
+  let dialog = document.getElementById('modal-shortcuts');
+  dialog.close();
+};
+
 const FigHeader = ({ state }) => (
   <div className="fig-header">
-    <i class="material-icons md-18">pageview</i>
-    <span class="header-text">{ state.figures[state.pi].name } - { state.figures[state.pi].list[state.ci].name }</span>
+    <i className="material-icons md-18">pageview</i>
+    <span className="header-text">{ state.figures[state.pi].name } - { state.figures[state.pi].list[state.ci].name }</span>
+    <div className="header-logo">
+      <a className="header-logo__link" href="https://github.com/t4y3/fig" target="_blank">GitHub</a>
+      <i className="header-logo__keybord material-icons" onclick={ openDialog }>keyboard</i>
+    </div>
+    <dialog id="modal-shortcuts" className="modal-shortcuts" onclick={ closeDialog }>
+      <p className="modal-shortcuts__title">Keyboard Shortcuts</p>
+      <p className="modal-shortcuts__item"><span>⌘ ⇧ ←</span>Toggle Tree View</p>
+      <p className="modal-shortcuts__item"><span>⌘ ⇧ →</span>Toggle Info View</p>
+      <p className="modal-shortcuts__item"><span>↑ or ↓</span>Change View Custom Tag</p>
+    </dialog>
   </div>
 )
 

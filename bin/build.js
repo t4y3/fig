5ab6c9a81673a7ee2aa20d25404101dc1331481c
Yuki Teraoka <y.teraoka8@gmail.com>
2019-04-24 00:43:38 +0900

    Fix: eslint errors

--- bin/build.js
+++ bin/build.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 // require the module as normal
-let fs = require('fs-extra');
-let InFig = require('../infig/main');
+const fs = require('fs-extra');
+const InFig = require('../infig/main');
 
 // Constant
 const PROCESS_CWD = process.cwd();
@@ -10,8 +10,7 @@ let config = {};
 let headHtml = '';
 
 // Dist dir
-const DIST_DIR = process.argv[2] ? `${ PROCESS_CWD }/${ process.argv[2] }` : PROCESS_CWD;
-
+const DIST_DIR = process.argv[2] ? `${PROCESS_CWD}/${process.argv[2]}` : PROCESS_CWD;
 
 /**
  * Load Config files
@@ -20,28 +19,26 @@ const loadConfigFile = () => {
   // reset figures list
   InFig.reset();
   // require config
-  config = require(`${ PROCESS_CWD }/.fig/config.js`);
-  for (let i = 0; i < config.figures.length; i++) {
-    require(`${ PROCESS_CWD }/${ config.figures[i] }.js`);
+  config = require(`${PROCESS_CWD}/.fig/config.js`); // eslint-disable-line global-require,import/no-dynamic-require
+  for (let i = 0; i < config.figures.length; i += 1) {
+    require(`${PROCESS_CWD}/${config.figures[i]}.js`); // eslint-disable-line global-require,import/no-dynamic-require
   }
   try {
-    headHtml = fs.readFileSync(`${ PROCESS_CWD }/.fig/head.html`, 'utf8');
+    headHtml = fs.readFileSync(`${PROCESS_CWD}/.fig/head.html`, 'utf8');
   } catch (err) {
     // Here you get the error when the file was not found,
     // but you also get any other error
   }
   config.headHtml = headHtml;
-  let c = Object.assign({}, config);
+  const c = Object.assign({}, config);
   c.figures = InFig.getFigures();
   if (config.bundle) {
-    fs.copyFileSync(config.bundle, `${ DIST_DIR }/bundle.js`);
+    fs.copyFileSync(config.bundle, `${DIST_DIR}/bundle.js`);
     c.bundle = 'bundle.js';
   }
-  fs.copySync(`${ __dirname }/../dist`, DIST_DIR);
-  fs.writeFile(`${ DIST_DIR }/fig.config.json`, JSON.stringify(c, null, '  '));
+  fs.copySync(`${__dirname}/../dist`, DIST_DIR);
+  fs.writeFile(`${DIST_DIR}/fig.config.json`, JSON.stringify(c, null, '  '));
 };
-
-
 
 /**
  * Load config
--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,26 @@
 #!/usr/bin/env node
 
 // require the module as normal
-let fs = require('fs-extra');
-let http = require('http');
-let browserSync = require("browser-sync");
-let InFig = require('../infig/main');
+const fs = require('fs-extra');
+const http = require('http');
+const browserSync = require('browser-sync');
+const InFig = require('../infig/main');
 
 // Constant
 const PROCESS_CWD = process.cwd();
 let config = {};
 let headHtml = '';
 
-
 /**
  * Delete cache
  */
 const deleteCache = () => {
-  delete require.cache[require.resolve(`${ PROCESS_CWD }/.fig/head.html`)];
-  delete require.cache[require.resolve(`${ PROCESS_CWD }/.fig/config.js`)];
-  for (let i = 0; i < config.figures.length; i++) {
-    delete require.cache[require.resolve(`${ PROCESS_CWD }/${ config.figures[i] }.js`)];
+  delete require.cache[require.resolve(`${PROCESS_CWD}/.fig/head.html`)];
+  delete require.cache[require.resolve(`${PROCESS_CWD}/.fig/config.js`)];
+  for (let i = 0; i < config.figures.length; i += 1) {
+    delete require.cache[require.resolve(`${PROCESS_CWD}/${config.figures[i]}.js`)];
   }
 };
-
 
 /**
  * Load Config files
@@ -31,23 +29,21 @@ const loadConfigFile = () => {
   // reset figures list
   InFig.reset();
   // require config
-  config = require(`${ PROCESS_CWD }/.fig/config.js`);
-  for (let i = 0; i < config.figures.length; i++) {
-    require(`${ PROCESS_CWD }/${ config.figures[i] }.js`);
+  config = require(`${PROCESS_CWD}/.fig/config.js`); // eslint-disable-line global-require,import/no-dynamic-require
+  for (let i = 0; i < config.figures.length; i += 1) {
+    require(`${PROCESS_CWD}/${config.figures[i]}.js`); // eslint-disable-line global-require,import/no-dynamic-require
   }
   try {
-    headHtml = fs.readFileSync(`${ PROCESS_CWD }/.fig/head.html`, 'utf8');
+    headHtml = fs.readFileSync(`${PROCESS_CWD}/.fig/head.html`, 'utf8');
   } catch (err) {
     // Here you get the error when the file was not found,
     // but you also get any other error
   }
   config.headHtml = headHtml;
-  let c = Object.assign({}, config);
+  const c = Object.assign({}, config);
   c.figures = InFig.getFigures();
-  fs.writeFile(`${ __dirname }/../dist/fig.config.json`, JSON.stringify(c, null, '  '));
+  fs.writeFile(`${__dirname}/../dist/fig.config.json`, JSON.stringify(c, null, '  '));
 };
-
-
 
 /**
  * Load config
@@ -55,28 +51,24 @@ const loadConfigFile = () => {
 loadConfigFile();
 deleteCache();
 
-
 // Start the server
 browserSync({
   port: 8080,
   files: [
-    `${ PROCESS_CWD }/${ config.bundle }`,
+    `${PROCESS_CWD}/${config.bundle}`,
     {
       match: [
-        `${ PROCESS_CWD }/**/fig.js`,
-        `${ PROCESS_CWD }/.fig/head.html`,
-        `${ PROCESS_CWD }/.fig/config.js`
+        `${PROCESS_CWD}/**/fig.js`,
+        `${PROCESS_CWD}/.fig/head.html`,
+        `${PROCESS_CWD}/.fig/config.js`,
       ],
-      /** Custom event handler **/
-      fn: (event, file) => {
+      /** Custom event handler * */
+      fn: (/* event, file */) => {
         loadConfigFile();
         deleteCache();
         browserSync.reload();
-      }
-    }
+      },
+    },
   ],
-  server: [
-    __dirname + "/../dist",
-    process.cwd()
-  ]
+  server: [`${__dirname}/../dist`, process.cwd()],
 });
